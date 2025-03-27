5f3b48cfd12c31d6ee04df6af4fb14374d18912d
AnthonyBustos16 <152900431+AnthonyBustos16@users.noreply.github.com>
2025-03-27 00:03:15 -0600

    bosta 3

--- paginaServicios/src/main.tsx
+++ paginaServicios/src/main.tsx
@@ -1,4 +1,4 @@
 import { createRoot } from "react-dom/client";
-import Nigga from "./MainPages";
+import Nigga from "./MainPages/Home";
 
 createRoot(document.getElementById("root")!).render(<Nigga />);
