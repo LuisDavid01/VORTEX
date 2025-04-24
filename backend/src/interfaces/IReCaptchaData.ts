507d941059e2483f1b06c5e90da2d958983bc955
Luis David <luisdavidmirandav12@gmail.com>
2025-04-24 14:08:35 -0600

    mejor uso javascript

--- backend/src/interfaces/IReCaptchaData.ts
+++
@@ -1,5 +0,0 @@
-export default interface ReCaptchaData{
-	secret: string;
-	response: string;
-
-}
--- backend/src/interfaces/IReCaptchaResponse.ts
+++
@@ -1,7 +0,0 @@
-export default interface ReCaptchaResponse{
-	success: boolean;
-	challenge_ts?: string;
-	hostname?: string;
-	'error-codes'?: string[];
-
-}
